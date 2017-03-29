db5a2b06164f6f6962e2a4fe607d2881acb212b4
Jeffrey Elkner <jeff@elkner.net>
2017-03-29 14:35:42 -0400

    Started a test for update

--- tests.py
+++ tests.py
@@ -112,6 +112,16 @@ class AppTests(unittest.TestCase):
 
         self.assertEqual(num_tasks_before-1, num_tasks_after)
 
+    def test_update_task_endpoint(self):
+        # Get tasks and mark one as done
+        self._add_items()
+        data = {}
+        headers = {'content-type': 'application/json'}
+        url = '/todo/api/v1.0/tasks'
+        response = self.client.put(url, headers=headers, data=data)
+        result = response.get_data(as_text=True)
+        tasks = json.loads(result)['tasks']
+
 
 if __name__ == '__main__':
     unittest.main()
